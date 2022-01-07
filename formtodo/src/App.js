40b006583e11147a7ad443702017979b1056a033
aishwaryaj20 <87424266+aishwaryaj20@users.noreply.github.com>
2022-01-07 18:13:47 +0530

    a

---
+++ formtodo/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+
+import TodoList from './components/TodoList';
+
+
+
+function App() {
+  return (
+    <div className='todo-app'>
+      <TodoList />
+  
+    </div>
+  );
+}
+
+export default App;
